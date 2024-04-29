805dc5db85ff92a885a7c89e8f4f572c656c3a4b
Thomas Trebat <thomastrebat@my.unt.edu>
2024-04-29 11:59:00 -0500

    normalized data with StandardScaler

--- dataset.py
+++ dataset.py
@@ -3,33 +3,40 @@ from torch.utils.data import Dataset
 from sklearn.preprocessing import StandardScaler
 
 class ApplianceEnergyUsageDataset(Dataset):
-    def __init__(self, energy_data_file, sequence_length, prediction_length):
+    def __init__(self, energy_data, sequence_length, prediction_length):
         super().__init__()
-        self.energy_data = pd.read_csv(energy_data_file)
+        self.energy_data = energy_data
         self.sequence_length = sequence_length
-        self.prediction_length = prediction_length
+        self.prediction_length = prediction_length        
 
     def __getitem__(self, index):
         sequence_begin = index
         sequence_end = sequence_begin + self.sequence_length
-        x = self.energy_data.iloc[sequence_begin:sequence_end, 1:3].values
+        x = self.energy_data[sequence_begin:sequence_end]
         prediction_begin = sequence_end
         prediction_end = prediction_begin + self.prediction_length
-        y = self.energy_data.iloc[prediction_begin:prediction_end, 1:3].values
+        y = self.energy_data[prediction_begin:prediction_end]
         return x, y
 
     def __len__(self):
-        return len(self.energy_data) - self.sequence_length - self.prediction_length + 1
-    
+        return len(self.energy_data) - self.sequence_length - self.prediction_length + 1    
+
+
 energy_data_file = '../../datasets/appliances_energy_prediction/energydata_complete.csv'
-sequence_length = 1
-prediction_length = 2
-dataset = ApplianceEnergyUsageDataset(energy_data_file, 
-                                      sequence_length, 
-                                      prediction_length)
-print(len(dataset))
-x, y = dataset[len(dataset) - 1]
-print('Sequence:')
-print(x)
-print('\nPrediction:')
-print(y)
+energy_data = pd.read_csv(energy_data_file, usecols=[1, 2])
+training_samples = int(len(energy_data) * 0.8)
+scaler = StandardScaler()
+train_data = scaler.fit_transform(energy_data.iloc[:training_samples].values)
+
+def get_train_dataset(sequence_length, prediction_length):
+    dataset = ApplianceEnergyUsageDataset(train_data, 
+                                          sequence_length, 
+                                          prediction_length)
+    return dataset
+
+def get_test_dataset(sequence_length, prediction_length):
+    test_data = scaler.transform(energy_data.iloc[training_samples - sequence_length:].values)
+    dataset = ApplianceEnergyUsageDataset(test_data,
+                                          sequence_length,
+                                          prediction_length)
+    return dataset
